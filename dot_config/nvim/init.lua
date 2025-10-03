2ad90122864ee7b85f51b164a1f1ba3266cc62dd
rozandoz <powerstatus524@gmail.com>
2025-10-03 14:52:21 +0200

    Update nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -14,6 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.lsp.enable('clangd')
+vim.lsp.enable({'clangd', 'rust_analyzer'})
 
 require("lazy").setup("plugins")
